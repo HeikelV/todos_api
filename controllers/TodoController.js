d61f3e569d5628ddd5bbe28ef2dc252e8f736259
Heikel Villar <heikelvillar@gmail.com>
2022-07-30 13:00:58 -0400

    change todo status

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -5,11 +5,12 @@ module.exports = {
   create,
   deleteTodo,
   update,
+  done,
 };
 
 async function getAll(_req, res) {
   try {
-    const todos = await Todo.query().orderBy('created_at', "desc");
+    const todos = await Todo.query().orderBy("created_at", "desc");
     res.status(200).json({
       data: todos,
     });
@@ -23,7 +24,7 @@ async function getAll(_req, res) {
 async function create(req, res) {
   try {
     const todo = await Todo.query().insert({
-      todo: req.body.todo
+      todo: req.body.todo,
     });
     res.status(200).json({
       data: todo,
@@ -54,7 +55,33 @@ async function update(req, res) {
   try {
     const { id } = req.params;
     const todo = await Todo.query().patchAndFetchById(id, {
-      todo: req.body.todo
+      todo: req.body.todo,
+    });
+
+    res.status(200).json({
+      data: todo,
+    });
+  } catch (error) {
+    res.status(500).json({
+      details: error.message,
+    });
+  }
+}
+
+async function done(req, res) {
+  try {
+    const { id } = req.params;
+
+    const result = await Todo.query().findById(id);
+
+    var new_status;
+    const _status = result.status;
+
+    new_status = _status === 'Pending' ? 'Done' : 'Pending';
+
+    const todo = await Todo.query().patchAndFetchById(id, {
+      todo: req.body.todo,
+      status: new_status,
     });
 
     res.status(200).json({
--- router.js
+++ router.js
@@ -10,6 +10,7 @@ router.get('/todos', TodoController.getAll);
 router.post('/todo', TodoController.create);
 router.delete('/todo/:id',TodoController.deleteTodo);
 router.put('/todo/:id',TodoController.update);
+router.patch('/todo/:id',TodoController.done);
 
 
 module.exports=router
